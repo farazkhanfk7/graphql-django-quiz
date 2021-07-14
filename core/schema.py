63f75f896d3b876377e3e9b03b0fe0ba4126166c
farazkhanfk7 <farazkhan138@gmail.com>
2021-07-14 14:29:15 +0530

    query args

--- core/schema.py
+++ core/schema.py
@@ -21,13 +21,24 @@ class QuestionType(DjangoObjectType):
 class AnswerType(DjangoObjectType):
     class Meta:
         model = Answer
-        fields = ("question","answer_text")
+        fields = ("question","answer_text","is_right")
 
 
 class Query(graphene.ObjectType):
     
-    all_quiz = DjangoListField(QuizType)
+    # all_quiz = DjangoListField(QuizType) this works as well
+    all_quiz = graphene.List(QuizType, id=graphene.Int())
+    all_answer = graphene.List(AnswerType, id=graphene.Int())
 
+    def resolve_all_quiz(root, info, id):
+        return Quiz.objects.filter(id=id)
+
+    def resolve_all_answer(root, info, id):
+        return Answer.objects.filter(id=id)
+
+
+    # def resolve_all_question(root, info):
+    #     return Question.objects.all()
 
 
 schema = graphene.Schema(query=Query)
